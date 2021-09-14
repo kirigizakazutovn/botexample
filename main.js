ec450ac1f1b500f985e93d82dca652ecde74f115
kirigizakazutovn <83147867+kirigizakazutovn@users.noreply.github.com>
2021-09-14 21:31:20 +0700

    main.js
    Main bot

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const Discord
+const client
+=
+require('discord.js')
+new Discord.Client()
+
+client.on('guildMemberAdd', member =>{
+
+member.guild.channels.cache.get('channel_id')
+send(welcome${member.user.username}: );
+
+});
+
+
+client.login ("BOT TOKEN")
 
